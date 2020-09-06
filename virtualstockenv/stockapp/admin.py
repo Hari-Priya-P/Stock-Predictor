6f52834b35786f5f1ec267324d11306a5cdf69f1
haripriya2703 <haripriyaponnekanti@gmail.com>
2020-09-06 00:14:49 -0400

    Stock Data

--- virtualstockenv/stockapp/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Historical, Stocks
-# Register your models here.
-
-admin.site.register(Stocks)
--- virtualstockenv/stockapp/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class StockAppConfig(AppConfig):
-    name = 'stockapp'
-
-    def ready(self):
-        from stockupdate import updater
-        updater.start()
